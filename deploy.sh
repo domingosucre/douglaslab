7f9f032009e380f2b5bd8995033902c1ecd47a22
kris <kris@stgabriel.io>
2026-08-10 17:19:32 +1000

    added some documentation to deploy script

--- deploy.sh
+++ deploy.sh
@@ -4,4 +4,14 @@ npm run build
 
 git add .
 git commit -am "commiting files to gh-pages"
+
+# this can break, when pushing from different machines"
+# and since gh-pages is pure build output with nothing worth preserving,
+# the simplest fix is to just wipe it and let subtree push recreate it fresh"
+
+# If you'd rather not delete history for some reason (unlikely to matter here, but worth naming as the alternative), you could instead do a force push:
+#
+# git push origin `git subtree split --prefix public`:gh-pages --force
+
+git push origin --delete gh-pages
 git subtree push --prefix public origin gh-pages
